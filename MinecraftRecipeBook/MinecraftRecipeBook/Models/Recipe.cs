5c9cd7af17f0274c5c3ccaa73c717b75a428e6a5
charl-neu <cshenkir@student.neumont.edu>
2026-02-24 15:54:27 -0700

    Implimented adding recipies

--- MinecraftRecipeBook/MinecraftRecipeBook/Models/Recipe.cs
+++ MinecraftRecipeBook/MinecraftRecipeBook/Models/Recipe.cs
@@ -5,7 +5,7 @@
         private static int nextId = 0;
         private readonly int id = nextId++;
         public int Id { get { return id; } }
-        public int[] Ingredients { get; set; } 
+        public int[] Ingredients { get; set; } = new int[9];
         public int Count { get; set; } = 1;
         public int Result { get; set; }
         public bool Shapeless { get; set; } = false;
