4ee40c391d31ea44a4e1e178ca0d27231bce1225
charl-neu <cshenkir@student.neumont.edu>
2026-03-03 14:40:21 -0700

    Implemented item adding & item viewing

--- MinecraftRecipeBook/MinecraftRecipeBook/obj/Debug/net10.0/MinecraftRecipeBook.AssemblyInfo.cs
+++ MinecraftRecipeBook/MinecraftRecipeBook/obj/Debug/net10.0/MinecraftRecipeBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinecraftRecipeBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9cd7af17f0274c5c3ccaa73c717b75a428e6a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f29729ef3d1f1663c282acbe41110afc9729d4e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinecraftRecipeBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinecraftRecipeBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
