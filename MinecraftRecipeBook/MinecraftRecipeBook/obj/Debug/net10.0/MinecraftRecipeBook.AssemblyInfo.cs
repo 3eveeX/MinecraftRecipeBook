7935a8bd3989a95b78e636fa3e0e5e598442e35e
charl-neu <cshenkir@student.neumont.edu>
2026-03-10 14:21:40 -0600

    Recreated item addition/removal

--- MinecraftRecipeBook/MinecraftRecipeBook/obj/Debug/net10.0/MinecraftRecipeBook.AssemblyInfo.cs
+++ MinecraftRecipeBook/MinecraftRecipeBook/obj/Debug/net10.0/MinecraftRecipeBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinecraftRecipeBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0b82f87d59f1cc2422286027cb5fad74eeb99c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3c452028e199bfe3e3471fb030530d4ee3630b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinecraftRecipeBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinecraftRecipeBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
